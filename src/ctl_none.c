2d0c7bcf5ab7f1ba5b96407ec30b63774604a28a
ivandeex <ivandeex@gmail.com>
2009-08-08 14:53:01 +0000

    remove obsolete sources

--- src/ctl_none.c
+++ src/ctl_none.c
@@ -20,12 +20,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
 
-#include <sys/poll.h>
-
 #include <alsa/asoundlib.h>
 #include <alsa/control_external.h>
 
-#include "none.h"
 
 typedef struct snd_ctl_none {
 	snd_ctl_ext_t ext;
